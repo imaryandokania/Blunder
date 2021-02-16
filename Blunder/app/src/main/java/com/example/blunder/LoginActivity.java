869044d2350043baa02b3abe977b06ac76277ce4
Aryan Dokania <aryandokania2001@gmail.com>
2021-02-16 20:32:53 +0545

    second

--- Blunder/app/src/main/java/com/example/blunder/LoginActivity.java
+++ Blunder/app/src/main/java/com/example/blunder/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.blunder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.background));
     }
 }
--- Blunder/app/src/main/java/com/example/blunder/MainActivity.java
+++ Blunder/app/src/main/java/com/example/blunder/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.blunder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.background));
         new CountDownTimer(3500, 1000) {
 
             @Override
